418b33c9afb176da6a89f5054df6af77c4f41238
psschiatanya <psschiatanya@gmail.com>
2024-11-18 23:56:35 +1100

    update inventory

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mrcloudbooktwotier" # Replace with your actual S3 bucket name
+    bucket = "s3-sivabucket" # Replace with your actual S3 bucket name
     key    = "EKS-Ajay/terraform.tfstate"
-    region = "ap-south-1"
+    region = "ap-southeast-2"
   }
 }
